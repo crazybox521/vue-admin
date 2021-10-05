9068d9ea9b41e2f51b3e9747859ae1648ae25064
crazybox521 <yunshu233@qq.com>
2021-10-05 20:14:16 +0800

    修改babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
+// 这是项目发布阶段需要用到的Babel插件
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins: ["transform-remove-console"]
+  ...prodPlugins
 }
